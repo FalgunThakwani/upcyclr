d20dd189e0366b7442cce4980b76591aa1d51fc5
Ronil Patel <rn373235@dal.ca>
2023-07-25 20:15:24 -0300

    Added Pickup history display page for the user

--- src/componenets/pickup-request/pickup-history.js
+++ src/componenets/pickup-request/pickup-history.js
@@ -8,21 +8,35 @@ const PickupRequests = () => {
   useEffect(() => {
     // Fetch the pickup requests data from the API
     const fetchData = async () => {
-      try {
-        const url = 'https://4h7b6re1c7.execute-api.us-east-1.amazonaws.com/dev/pickup-request/1234934934';
-        const response = await axios.get(url);
-        console.log(response)
-        const { data } = response.data;
-        
-        setPickupRequests(data);
-      } catch (error) {
-        console.error('API Error:', error);
-      }
+        try {
+            const url = 'https://324q8p7pd4.execute-api.us-east-1.amazonaws.com/dev/pickup-request/1234934934';
+            
+            // Make the fetch request
+            const response = await fetch(url);
+            
+            // Check if the response was successful (status code 200-299)
+            if (!response.ok) {
+              throw new Error(`API Error: ${response.status} ${response.statusText}`);
+            }
+
+            // Parse the response JSON
+            const data = await response.json();
+            console.log(data);
+            console.log(data.data)
+            
+            // Assuming 'data' contains the 'Items' array from the response
+            setPickupRequests(data.data);
+            console.log(pickupRequests)
+          } catch (error) {
+            console.error('API Error:', error);
+          }
+          
     };
 
     fetchData();
   }, []);
 
+  
   return (
     <div>
       <Typography variant="h4" align="center" gutterBottom>
@@ -37,12 +51,12 @@ const PickupRequests = () => {
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell>ID</TableCell>
                 <TableCell>User Name</TableCell>
                 <TableCell>City</TableCell>
                 <TableCell>Street</TableCell>
                 <TableCell>Unit</TableCell>
                 <TableCell>Postal Code</TableCell>
+                <TableCell>Items in Bag</TableCell>
                 <TableCell>Pickup Start Time</TableCell>
                 <TableCell>Pickup End Time</TableCell>
                 <TableCell>Reward</TableCell>
@@ -52,12 +66,20 @@ const PickupRequests = () => {
             <TableBody>
               {pickupRequests.map((request) => (
                 <TableRow key={request.id}>
-                  <TableCell>{request.id}</TableCell>
                   <TableCell>{request.user.name}</TableCell>
                   <TableCell>{request.pickup_address.city}</TableCell>
                   <TableCell>{request.pickup_address.street}</TableCell>
                   <TableCell>{request.pickup_address.unit}</TableCell>
                   <TableCell>{request.pickup_address.postal}</TableCell>
+                  <TableCell>
+                    {request.items_in_bag.length > 0 ? (
+                      <ul>
+                        {request.items_in_bag.map((item, index) => (
+                          <li key={index}>{`${item.weight} ${item.unit} of ${item.type}`}</li>
+                        ))}
+                      </ul>
+                    ) : "None"}
+                  </TableCell>
                   <TableCell>{request.pickup_start_ts}</TableCell>
                   <TableCell>{request.pickup_end_ts}</TableCell>
                   <TableCell>{request.reward}</TableCell>
@@ -73,3 +95,4 @@ const PickupRequests = () => {
 };
 
 export default PickupRequests;
+
